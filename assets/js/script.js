4887cecd3df70b2d839f7bed80cfe3b00d7e6713
Cyril Leblanc <cyril.leblanc@outlook.com>
2024-03-04 08:59:33 -0600

    add opacity on toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,5 @@ const navbar = document.querySelector(".nav__link");
 
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("nav__mobile");
+  hamburger.style.opacity = hamburger.style.opacity === "1" ? "0.6" : "1";
 });
